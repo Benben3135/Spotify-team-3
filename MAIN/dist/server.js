68e63f5c1a91ea1073b5da2459429672305e782d
Benjamin Volodarsky <127942178+Benben3135@users.noreply.github.com>
2023-09-25 01:11:51 +0300

    .

--- MAIN/dist/server.js
+++ MAIN/dist/server.js
@@ -6,10 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const multer_gridfs_storage_1 = require("multer-gridfs-storage");
+//streaming library for node.js
+const path = require("path");
+const crypto = require("crypto");
+const multer = require("multer");
+const Grid = require("gridfs-stream");
+const bodyParser = require("body-parser");
 //npm i dotenv
 require("dotenv/config");
 const app = express_1.default();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 //middlware for using parser
 app.use(cookie_parser_1.default());
 //static files
@@ -24,6 +32,62 @@ mongoose_1.default.connect(MONGO_URI).then(() => {
     .catch(err => {
     console.error(err);
 });
+//setting the songs DB:
+const { SONGS_MONGO_URI } = process.env;
+const conn = mongoose_1.default.createConnection(SONGS_MONGO_URI);
+let gfs;
+conn.once('open', () => {
+    //init stream
+    gfs = Grid(conn.db, mongoose_1.default.mongo);
+    gfs.collection('uploads');
+});
+//create storage engine
+const storage = new multer_gridfs_storage_1.GridFsStorage({
+    url: SONGS_MONGO_URI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString("hex") + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: "uploads",
+                };
+                resolve(fileInfo);
+            });
+        });
+    },
+});
+const upload = multer({ storage });
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.redirect("/");
+});
+app.get("/play/:filename", (req, res) => {
+    const filename = req.params.filename;
+    gfs.files.findOne({ filename }, (err, file) => {
+        if (err || !file) {
+            return res.status(404).json({
+                err: "File not found",
+            });
+        }
+        // Check if the file is an MP3
+        if (file.contentType !== "audio/mpeg" && file.contentType !== "audio/mp3") {
+            return res.status(400).json({
+                err: "Not an MP3 file",
+            });
+        }
+        // Stream the file to the client
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+    });
+});
+//to get the audio:
+// <audio controls>
+//   <source src="/play/your_mp3_filename.mp3" type="audio/mpeg">
+//   Your browser does not support the audio element.
+// </audio>
 const userRouter_1 = __importDefault(require("./API/users/userRouter"));
 app.use("/API/users", userRouter_1.default);
 app.listen(port, () => {
--- MAIN/server.ts
+++ MAIN/server.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import { GridFsStorage } from 'multer-gridfs-storage';
+
+//streaming library for node.js
+const path = require("path");
+const crypto = require("crypto");
+const multer = require("multer");
+const Grid = require("gridfs-stream");
+const bodyParser = require("body-parser");
+
 
 //npm i dotenv
 import 'dotenv/config'
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
 
 
 //middlware for using parser
@@ -28,6 +40,76 @@ mongoose.connect(MONGO_URI).then(()=>{
 .catch(err=>{
   console.error(err)
 })
+
+//setting the songs DB:
+const {SONGS_MONGO_URI} = process.env;
+const conn = mongoose.createConnection(SONGS_MONGO_URI);
+let gfs;
+conn.once('open', () => {
+  //init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+//create storage engine
+const storage = new GridFsStorage({
+  url: SONGS_MONGO_URI,
+  file: (req: any, file: any) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err: any, buf: any) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("file"), (req: any, res: any) => {
+  res.redirect("/")
+});
+
+
+app.get("/play/:filename", (req, res) => {
+  const filename = req.params.filename;
+
+  gfs.files.findOne({ filename }, (err, file) => {
+    if (err || !file) {
+      return res.status(404).json({
+        err: "File not found",
+      });
+    }
+
+    // Check if the file is an MP3
+    if (file.contentType !== "audio/mpeg" && file.contentType !== "audio/mp3") {
+      return res.status(400).json({
+        err: "Not an MP3 file",
+      });
+    }
+
+    // Stream the file to the client
+    const readstream = gfs.createReadStream(file.filename);
+    readstream.pipe(res);
+  });
+});
+
+//to get the audio:
+// <audio controls>
+//   <source src="/play/your_mp3_filename.mp3" type="audio/mpeg">
+//   Your browser does not support the audio element.
+// </audio>
+
+
+
+
+
 
 
 import userRouter from "./API/users/userRouter";
